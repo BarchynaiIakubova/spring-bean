82985208a9d61dc2020f826b5419ebbadd9aaf50
barchynaiiakubova <barchynai.iakubova@gmail.com>
2022-05-15 17:16:27 +0600

    barchynaisHW

--- src/main/java/peaksoft/App.java
+++ src/main/java/peaksoft/App.java
@@ -10,5 +10,19 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        System.out.println("Comparing Helloworld bean 1 and bean 2: " + (bean == bean2));
+
+        Cat catBean =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getName());
+
+        Cat catBean2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getName());
+        System.out.println("Comparing Cat objects bean1 and bean2: " + (catBean == catBean2));
     }
 }
